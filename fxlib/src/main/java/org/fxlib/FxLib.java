1986e9de5dee1ada6ab2030aecd390ebf6b0ef26
rzakaan <rzakaan@gmail.com>
2022-01-12 13:10:42 +0000

    Incorret class name update and add showScene

--- fxlib/src/main/java/org/fxlib/FxLib.java
+++ fxlib/src/main/java/org/fxlib/FxLib.java
@@ -1,7 +1,67 @@
 package org.fxlib;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.lang.reflect.Method;
+
 public class FxLib {
-    public boolean someLibraryMethod() {
-        return true;
+    public static void showScene(String fxml, Image icon, Object data) {
+        try {
+            String path = "/fxml/" + fxml;
+            final double[] xOffset = {0};
+            final double[] yOffset = {0};
+
+            FXMLLoader loader = new FXMLLoader(FxLib.class.getResource(path));
+            loader.setLocation(FxLib.class.getResource(path));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));            
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setOnShown(e->{
+                try {
+                    Method m = loader.getController().getClass().getDeclaredMethod("onShown", Object.class);
+                    if (m != null) m.invoke(loader.getController(), data);
+                } catch (NoSuchMethodException ee) {
+                } catch (Exception ee) {
+                    ee.printStackTrace();
+                }
+            });
+            
+            if (null != icon) {
+                try {
+                    stage.getIcons().add(icon);    
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            stage.show();
+
+            root.setOnMousePressed(event -> {
+                xOffset[0] = event.getSceneX();
+                yOffset[0] = event.getSceneY();
+            });
+
+            root.setOnMouseDragged(event -> {
+                stage.setX(event.getScreenX() - xOffset[0]);
+                stage.setY(event.getScreenY() - yOffset[0]);
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void showScene(String fxml, Image icon) {
+        showScene(fxml, icon, null);
+    }
+    
+    public static void showScene(String fxml) {
+        showScene(fxml, null, null);
     }
 }
--- fxlib/src/main/java/org/fxlib/table/TableViewController.java
+++ fxlib/src/main/java/org/fxlib/table/TableViewController.java
@@ -1,5 +1,46 @@
 package org.fxlib.table;
 
-public class TableViewController {
-    public abstract String columnTitle(String field);
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public abstract class TableViewController {
+    public static void init(TableView<Object> table, TableViewModel model) {
+        if (null == table) {
+            return;
+        }
+
+        Class clazz = model.getClass();
+        table.getColumns().clear();
+        
+        for (Field field : clazz.getFields()) {
+            String columnName = model.getColumnTitle(field.getName()) == null ? field.getName() : model.columnTitle(field.getName());
+            TableColumn<Object, Object> column = new TableColumn<>(columnName);
+            column.setCellValueFactory(param -> {
+                try {
+                    Object object = model.getClass().getField(field.getName()).get(param.getValue());
+                    if (object instanceof Double || object instanceof Float) {
+                        return new SimpleObjectProperty<>(String.format("%.2f", object));
+                    } else {
+                        return new SimpleObjectProperty<>(object);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return new SimpleObjectProperty<>("-");
+            });
+
+            table.getColumns().add(column);
+        }
+
+        for (int i = 0; i < table.getColumns().size(); i++) {
+            table.getColumns().get(i).prefWidthProperty().bind(table.widthProperty().divide(table.getColumns().size()));
+        }
+    }
+
+    public static void init(TableView<Object> table, TableViewModel model, ObservableList list) {
+        TableViewModel.init(table, model);
+        table.setItems(list);
+    }
 }
